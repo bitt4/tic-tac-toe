707bc3cf47860afcc757ed38a66dcba1c7aff621
bitt4 <jakub.bittara@gmail.com>
2020-07-25 22:52:41 +0200

    click on cell, drawCross, some properties

--- Tictactoe.hpp
+++ Tictactoe.hpp
@@ -1,9 +1,18 @@
 #include <SDL2/SDL.h>
+#include <SDL2/SDL2_gfxPrimitives.h>
 
 class Tictactoe{
 private:
     SDL_Renderer *renderer;
+    int boardWidth;
+    int boardHeight;
+    int cells;
+    int cell_width;
+    SDL_Color crossColor;
+
+    void drawCross(int, int);
 public:
-    Tictactoe(SDL_Renderer*);
+    Tictactoe(SDL_Renderer*, int, int);
     void renderInit();
+    void click_on_cell(int, int);
 };
