1afb5ba08a6771e820a61ab5ca1ad19d155c117e
bitt4 <64093718+bitt4@users.noreply.github.com>
2020-07-25 22:53:51 +0200

    click on cell function, render cross

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -1,7 +1,13 @@
 #include "Tictactoe.hpp"
 
-Tictactoe::Tictactoe(SDL_Renderer *renderer){
+Tictactoe::Tictactoe(SDL_Renderer *renderer, int w, int h)
+    :cells(3),
+     crossColor({.r = 0, .g = 0, .b = 255})
+{
         this->renderer = renderer;
+        this->boardWidth = w;
+        this->boardHeight = h;
+        this->cell_width = this->boardWidth / this->cells;
 }
 
 void Tictactoe::renderInit(){
@@ -13,6 +19,48 @@ void Tictactoe::renderInit(){
     SDL_RenderDrawLine(renderer, 402, 0, 402, 602);
     SDL_RenderDrawLine(renderer, 0, 201, 602, 201);
     SDL_RenderDrawLine(renderer, 0, 402, 602, 402);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Tictactoe::click_on_cell(int x, int y){
+    /*
+    SDL_RenderDrawLine(renderer,
+                       x * cell_width + cell_width * 0.5,
+                       y * cell_width + cell_width * 0.5,
+                       x * cell_width + cell_width * 0.5,
+                       y * cell_width + cell_width * 0.5
+                       );
+    */
+
+    drawCross(x * cell_width + cell_width * 0.5,
+              y * cell_width + cell_width * 0.5);
+
+    SDL_RenderPresent(renderer);
+}
+
+void Tictactoe::drawCross(int x, int y){
+    thickLineRGBA(renderer,
+                  x - cell_width * 0.3,
+                  y + cell_width * 0.3,
+                  x + cell_width * 0.3,
+                  y - cell_width * 0.3,
+                  10,
+                  0,
+                  0,
+                  255,
+                  255);
+
+    thickLineRGBA(renderer,
+                  x - cell_width * 0.3,
+                  y - cell_width * 0.3,
+                  x + cell_width * 0.3,
+                  y + cell_width * 0.3,
+                  10,
+                  crossColor.r,
+                  crossColor.g,
+                  crossColor.b,
+                  255);
 
     SDL_RenderPresent(renderer);
 }
