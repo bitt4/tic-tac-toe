9a0200bb64adfe13fb284a87ba446b841641d85b
bitt4 <jakub.bittara@gmail.com>
2020-07-26 23:51:45 +0200

    getSize and drawCircle

--- Tictactoe.cpp
+++ Tictactoe.cpp
@@ -1,13 +1,18 @@
 #include "Tictactoe.hpp"
 
 Tictactoe::Tictactoe(SDL_Renderer *renderer, int w, int h)
-    :cells(3),
-     crossColor({.r = 0, .g = 0, .b = 255})
+    :size(3),
+     crossColor({.r = 0, .g = 0, .b = 255}),
+     circleColor({.r = 255, .g = 0, .b = 0})
 {
         this->renderer = renderer;
         this->boardWidth = w;
         this->boardHeight = h;
-        this->cell_width = this->boardWidth / this->cells;
+        this->cell_width = this->boardWidth / this->size;
+}
+
+int Tictactoe::getSize(){
+    return this->size;
 }
 
 void Tictactoe::renderInit(){
@@ -24,17 +29,13 @@ void Tictactoe::renderInit(){
 }
 
 void Tictactoe::click_on_cell(int x, int y){
-    /*
-    SDL_RenderDrawLine(renderer,
-                       x * cell_width + cell_width * 0.5,
-                       y * cell_width + cell_width * 0.5,
-                       x * cell_width + cell_width * 0.5,
-                       y * cell_width + cell_width * 0.5
-                       );
-    */
-
     drawCross(x * cell_width + cell_width * 0.5,
               y * cell_width + cell_width * 0.5);
+
+    /*
+    drawCircle(x * cell_width + cell_width * 0.5,
+               y * cell_width + cell_width * 0.5);
+    */
 
     SDL_RenderPresent(renderer);
 }
@@ -63,4 +64,24 @@ void Tictactoe::drawCross(int x, int y){
                   255);
 
     SDL_RenderPresent(renderer);
+}
+
+void Tictactoe::drawCircle(int x, int y){
+    filledCircleRGBA(renderer,
+                     x,
+                     y,
+                     cell_width * 0.4,
+                     circleColor.r,
+                     circleColor.g,
+                     circleColor.b,
+                     255);
+
+    filledCircleRGBA(renderer,
+                     x,
+                     y,
+                     cell_width * 0.35,
+                     0,
+                     0,
+                     0,
+                     255);
 }
