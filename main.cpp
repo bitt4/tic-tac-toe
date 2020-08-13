3547925e5130b6cbf1551bfec76363b54c923dde
bitt4 <jakub.bittara@gmail.com>
2020-08-13 21:35:09 +0200

    Program exit after help

--- main.cpp
+++ main.cpp
@@ -18,6 +18,7 @@ int main(int argc, char *argv[]){
         for(int i = 1; i < argc; i++){
             if(strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0){
                 displayHelp();
+		return EXIT_SUCCESS;
             }
 
             if(strcmp(argv[i], "-s") == 0 || strcmp(argv[i], "--size") == 0){
